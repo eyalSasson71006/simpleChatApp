3fb7418a49fef16820e9672c1a0b0c319c5c96fe
eyal71006 <eyal71006@gmail.com>
2024-09-29 09:52:34 +0300

    basic connection and html form

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const socketIo = require('socket.io');
 const app = express();
 
-app.use(express.json());
 const PORT = 8181;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');   
+const server = app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-})
+const io = socketIo(server);
+
+io.on("connection", (stream) => {
+    console.log("User connected");
+
+    stream.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
