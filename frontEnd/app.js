b42d1391e4d9a1cf4ed678fbc7ae2c613a846c16
eyal71006 <eyal71006@gmail.com>
2024-09-29 12:36:34 +0300

    username is session storage and better design

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,18 +1,40 @@
 const socket = io("http://localhost:8181");
 
-const printMessage = (msg) => {
-    const chatWindow = document.getElementById("chatWindow");
-    let message = document.createElement("p");
-    message.innerText = msg;
+const chatWindow = document.getElementById("chatWindow");
+const sendBtn = document.getElementById("sendBtn");
+const messageInput = document.getElementById("messageInput");
+const usernameInput = document.getElementById("usernameInput");
+sendBtn.disabled = true;
+usernameInput.value = sessionStorage.getItem("username") || "";
+
+function printMessage(msg) {
+    const { username, messageText, time } = msg;
+    let message = document.createElement("div");
+    message.innerHTML = `<span class="username">${username}</span> <span class="time">${time}</span> <span class="message">${messageText}</span>`;
     chatWindow.appendChild(message);
 };
 
-const sendBtn = document.getElementById("sendBtn");
 sendBtn.addEventListener("click", () => {
-    const messageInput = document.getElementById("messageInput");
-    socket.emit("message sent", messageInput.value);
+    const now = new Date();
+    if (!messageInput.value || !usernameInput.value) return;
+    socket.emit("message sent", { username: usernameInput.value, messageText: messageInput.value, time: now.getHours() + ":" + now.getMinutes() });
+    messageInput.value = "";
+    sendBtn.disabled = true;
+    messageInput.focus();
 });
+
+messageInput.addEventListener("input", () => {
+    if (messageInput.value && usernameInput.value) sendBtn.disabled = false;
+    else sendBtn.disabled = true;
+});
+
+usernameInput.addEventListener("input", () => {
+    sessionStorage.setItem("username", usernameInput.value);
+    if (usernameInput.value && messageInput.value) sendBtn.disabled = false;
+    else sendBtn.disabled = true;
+});
+
 
 socket.on("message received", (message) => {
     printMessage(message);
-})
+});
